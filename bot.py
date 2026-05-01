8762fb729c8d73e96a25e88b45b5a64ccd5bb9aa
szeyongk <szeyongk@gmail.com>
2026-05-01 17:40:46 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import time
 from datetime import datetime
 import pytz
 
-ANTHROPIC_API_KEY = os.environ.get(‘ANTHROPIC_API_KEY’)
-TELEGRAM_BOT_TOKEN = os.environ.get(‘TELEGRAM_BOT_TOKEN’)
-TELEGRAM_CHAT_ID = os.environ.get(‘TELEGRAM_CHAT_ID’)
+ANTHROPIC_API_KEY = os.environ.get(chr(39)+chr(65)+chr(78)+chr(84)+chr(72)+chr(82)+chr(79)+chr(80)+chr(73)+chr(67)+chr(95)+chr(65)+chr(80)+chr(73)+chr(95)+chr(75)+chr(69)+chr(89)+chr(39))
+TELEGRAM_BOT_TOKEN = os.environ.get(chr(39)+chr(84)+chr(69)+chr(76)+chr(69)+chr(71)+chr(82)+chr(65)+chr(77)+chr(95)+chr(66)+chr(79)+chr(84)+chr(95)+chr(84)+chr(79)+chr(75)+chr(69)+chr(78)+chr(39))
+TELEGRAM_CHAT_ID = os.environ.get(chr(39)+chr(84)+chr(69)+chr(76)+chr(69)+chr(71)+chr(82)+chr(65)+chr(77)+chr(95)+chr(67)+chr(72)+chr(65)+chr(84)+chr(95)+chr(73)+chr(68)+chr(39))
 
 SEND_HOUR = 16
 SEND_MINUTE = 0
